9deec2cba2bf3026aff9386a7a0d9dd6b7b3e5e8
mozieblo <m.ozieblo@wp.pl>
2019-06-24 08:44:23 +0200

    update

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -5,6 +5,20 @@ var budgetController = (function(){
         this.id = id;
         this.description = description;
         this.value = value;
+        this.percentage = -1;
+    };
+
+    Expense.prototype.calcPercentage = function(totalIncome){
+
+        if(totalIncome > 0){
+            this.percentage = Math.round((this.value / totalIncome)*100);
+        } else {
+            this.percentage = -1;
+        }
+    };
+
+    Expense.prototype.getPercentage = function(){
+        return this.percentage;
     };
 
     var Income = function(id, description, value){
@@ -85,6 +99,20 @@ var budgetController = (function(){
                 data.percentage = -1;
             }
 
+        },
+
+        calculatePercentages: function(){
+
+            data.allItems.exp.forEach(function(cur){
+                cur.calcPercentage(data.totals.inc);
+            })
+        },
+
+        getPercentages: function (){
+            var allPerc = data.allItems.exp.map(function(cur){
+                return cur.getPercentage();
+            });
+            return allPerc;
         },
 
         getBudget: function (){
@@ -231,6 +259,13 @@ var controller = (function(budgetCtrl, UICtrl){
 
     };
 
+    var updatePercentage = function(){
+        budgetCtrl.calculatePercentages();
+        var percentages = budgetCtrl.getPercentages();
+        console.log(percentages);
+
+    };
+
     var ctrlAddItem = function(){
         var input, newItem;
         input = UICtrl.getInput();
@@ -240,6 +275,7 @@ var controller = (function(budgetCtrl, UICtrl){
             UICtrl.addListItem(newItem, input.type);
             UICtrl.clearFields();
             updateBudget();
+            updatePercentage();
         }
     };
 
@@ -255,6 +291,7 @@ var controller = (function(budgetCtrl, UICtrl){
             budgetCtrl.deleteItem(type, ID);
             UICtrl.deleteListItem(itemID);
             updateBudget();
+            updatePercentage();
         }
 
 
