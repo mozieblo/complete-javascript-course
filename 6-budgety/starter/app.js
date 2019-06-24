08cf0c5e27b1eca936f58c6ed8612255bd5434ac
mozieblo <m.ozieblo@wp.pl>
2019-06-24 14:03:35 +0200

    finished

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -276,13 +276,25 @@ var UIController = (function(){
                 document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
             },
 
+            changedType: function(){
+
+                var fields = document.querySelectorAll(
+                    DOMstrings.inputType + ',' +
+                    DOMstrings.inputDescription + ','+
+                    DOMstrings.inputValue
+                );
+
+                nodeListForEach(fields, function(cur){
+                    cur.classList.toggle('red-focus');
+                });
+
+                document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
+            },
+
             getDOMstrings: function(){
                 return DOMstrings
             }
         }
-
-
-
 })();
 
 
@@ -301,6 +313,8 @@ var controller = (function(budgetCtrl, UICtrl){
         });
 
         document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
+        document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
     };
 
     var updateBudget = function(){
